cb6f5080a827074d428dbb749eb88913a0ade72f
tomrihoy <60923992+tomrihoy@users.noreply.github.com>
2026-04-21 09:26:55 +0100

    added delta exposure and hedge coverage, refactored code

--- trade_aggregator.py
+++ trade_aggregator.py
@@ -1,6 +1,7 @@
 from load_trades import load_trades_from_json, TradeCommodityType, TradeBook, Trade
 import pandas as pd
 from dataclasses import dataclass
+import numpy as np
 
 @dataclass(frozen=True, slots=True)
 class TradePositionAggregation:
@@ -10,6 +11,18 @@ class TradePositionAggregation:
     net_position: float
     trade_count: int
     total_cost: float
+
+@dataclass(frozen=True, slots=True)
+class DeltaExposure:
+    commodity: TradeCommodityType
+    delivery_period: str
+    delta_exposure: float
+
+@dataclass(frozen=True, slots=True)
+class HedgeCoverage:
+    commodity: TradeCommodityType
+    delivery_period: str
+    hedge_coverage: float
 
 def trades_to_df(trade_list: list[Trade])->pd.DataFrame:
 
@@ -24,7 +37,7 @@ def trades_to_df(trade_list: list[Trade])->pd.DataFrame:
 
     return df
 
-def aggregate_trades(trade_list: list[Trade])->list[TradePositionAggregation]:
+def aggregate_trades(trade_list: list[Trade])->pd.DataFrame:
     trade_df = trades_to_df(trade_list)
 
     aggregated_trades = trade_df.groupby(
@@ -34,6 +47,11 @@ def aggregate_trades(trade_list: list[Trade])->list[TradePositionAggregation]:
         trade_count=('signed_qty', 'count'),
         total_cost=('cost', 'sum')
     ).reset_index()
+    return aggregated_trades
+
+
+def position_aggregations(trade_list: list[Trade])->list[TradePositionAggregation]:
+    aggregated_trades = aggregate_trades(trade_list)
     
     position_aggregations=[]
     
@@ -50,10 +68,56 @@ def aggregate_trades(trade_list: list[Trade])->list[TradePositionAggregation]:
         )
     return position_aggregations
 
+def delta_exposure(trade_list: list[Trade])->pd.DataFrame:
+    aggregated_trades = aggregate_trades(trade_list)
+    delta_df = aggregated_trades.pivot_table( index=["commodity", "delivery_period"], columns="book",
+    values="net_position",
+    fill_value=0, ).reset_index()
+    delta_df["delta"] = (
+    delta_df.get("physical", 0) + delta_df.get("hedge", 0)
+    )
+    delta_exposures=[]
+    for row in delta_df.itertuples():
+        delta_exposures.append(
+            DeltaExposure(
+                commodity=row.commodity,
+                delivery_period=row.delivery_period,
+                delta_exposure=row.delta)
+        )
+    return delta_exposures
+    
+def hedge_coverage(trade_list:list[Trade])->pd.DataFrame:
+    aggregated_trades = aggregate_trades(trade_list)
+    hedge_coverage_df = aggregated_trades.pivot_table( index=["commodity", "delivery_period"], columns="book",
+    values="net_position",
+    fill_value=0, ).reset_index()
+    hedge_coverage_df["hedge_coverage"] = (
+    100*np.abs(hedge_coverage_df.get("hedge", 0))/np.abs(hedge_coverage_df.get("physical", 0))
+    )
+    hedge_coverages=[]
+    for row in hedge_coverage_df.itertuples():
+        hedge_coverages.append(
+            HedgeCoverage(
+                commodity=row.commodity,
+                delivery_period=row.delivery_period,
+                hedge_coverage=row.hedge_coverage)
+        )
+    return hedge_coverages
+    
+    
+    
+    return hedge_coverage_df
+
+
 if __name__ == '__main__':
     trade_list = load_trades_from_json(r'trades.json')
-    aggregated_trades = aggregate_trades(trade_list)
+    aggregated_trades = position_aggregations(trade_list)
+    deltas = delta_exposure(trade_list)
+    hedge_coverages = hedge_coverage(trade_list)
     
+
+
+
     header = f"{'Book':<15} {'Commodity':<15} {'Delivery Period':<20} {'Net Position':>15} {'Trade Count':>12} {'Total Cost':>15}"
     print(header)
     print('-' * len(header))
